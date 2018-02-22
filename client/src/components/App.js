b705148fd47daee640ab687a5ea9d436c471cf18
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-02-21 20:37:34 -0800

    initial experimentation

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import '../styles/index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Zimmerbot</h1>
+        <p className="Wikimedia"> Wikimedia Foundation </p>
+      </div>
+    );
+  }
+}
+
+export default App;

095a04391b3e4e26eb080c4228cdc0af627d419f
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-08 23:25:21 -0800

    more header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Image } from 'semantic-ui-react';
 import wiki_logo from '../wiki_logo.png';
 
 class Header extends React.Component {
@@ -12,12 +12,36 @@ class Header extends React.Component {
     return (
       <div className="header">
         <Menu size="large" widths="5" borderless className="menu">
-          <Menu.Item header>
+          <Menu.Item>
+            <h1>ZimmerBot</h1>
+          </Menu.Item>
+          <Menu.Item
+            header
+            name="home"
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          >
             <Icon name="home" />
           </Menu.Item>
-          <Menu.Item>About Us</Menu.Item>
-          <Menu.Item header>
+          <Menu.Item
+            header
+            name="about"
+            active={activeItem === 'about'}
+            onClick={this.handleItemClick}
+          >
+            About Us
+          </Menu.Item>
+          <Menu.Item
+            header
+            header
+            name="libraries"
+            active={activeItem === 'libraries'}
+            onClick={this.handleItemClick}
+          >
             <Icon name="sidebar" />
+          </Menu.Item>
+          <Menu.Item>
+            <Image src={wiki_logo} />
           </Menu.Item>
         </Menu>
       </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,9 +5,17 @@ import { Button } from 'semantic-ui-react';
 class Home extends React.Component {
   render() {
     return (
-      <div className="home">
-        <SearchBar />
-        <Button id="search-button" circular icon="search" />
+      <div>
+        <div className="home">
+          <SearchBar />
+          <Button
+            id="search-button"
+            circular
+            color="green"
+            icon="search"
+            size="large"
+          />
+        </div>
       </div>
     );
   }

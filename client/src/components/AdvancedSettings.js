2b826c132dd00524ac7ec6b596a35a848ef369a0
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-12 01:32:21 -0700

    starting advanced settings form

---
+++ client/src/components/AdvancedSettings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form } from 'semantic-ui-react';
+
+
+class AdvancedSettings extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default AdvancedSettings;
